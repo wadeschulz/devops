f42e7827c2882cb143d36447b4385e2a6c4f887c
Wade Schulz, MD, PhD <wade.schulz@gmail.com>
2016-01-02 12:25:14 -0500

    add duplicate instance creation to packer-driver
    probably easier and faster to do a server-side duplication, rather than
    recreating each image from scratch

--- packer/packer-driver.py
+++ packer/packer-driver.py
@@ -7,22 +7,25 @@ esxi_server = raw_input('ESXI server address: ')
 esxi_username = raw_input('ESXI SSH username: ')
 esxi_password = getpass.getpass('ESXI SSH password: ')
 datastore = raw_input('Datastore name: ')
-vm_name = raw_input('VM/Build name: ')
+vm_base_name = raw_input('VM/Build name: ')
+num_instances = int(raw_input('Number instances: '))
 
-packer_ip = " -var 'ESXI_IP=" + esxi_server + "'"
-packer_user = " -var 'ESXI_USER=" + esxi_username + "'"
-packer_pass = " -var 'ESXI_PASS=" + esxi_password + "'"
-packer_datastore = " -var 'DATASTORE=" + datastore + "'"
-packer_vm_name = " -var 'VM_NAME=" + vm_name + "'"
+for i in range(1, num_instances+1):
+	vm_name = vm_base_name + "-" + str(i)
+	packer_ip = " -var 'ESXI_IP=" + esxi_server + "'"
+	packer_user = " -var 'ESXI_USER=" + esxi_username + "'"
+	packer_pass = " -var 'ESXI_PASS=" + esxi_password + "'"
+	packer_datastore = " -var 'DATASTORE=" + datastore + "'"
+	packer_vm_name = " -var 'VM_NAME=" + vm_name + "'"
 
-os.chdir(build_type)
+	os.chdir(build_type)
 
-os.system("/opt/packer/packer build" + packer_ip + packer_user + packer_pass + packer_datastore + packer_vm_name + " packer-config.json")
+	os.system("/opt/packer/packer build" + packer_ip + packer_user + packer_pass + packer_datastore + packer_vm_name + " packer-config.json")
 
-ssh = SSHClient()
-ssh.load_system_host_keys()
-ssh.connect(esxi_server, username=esxi_username, password=esxi_password)
+	ssh = SSHClient()
+	ssh.load_system_host_keys()
+	ssh.connect(esxi_server, username=esxi_username, password=esxi_password)
 
-register_vm = "vim-cmd solo/registervm /vmfs/volumes/" + datastore + "/output-" + vm_name + "/packer-" + vm_name + ".vmx"
+	register_vm = "vim-cmd solo/registervm /vmfs/volumes/" + datastore + "/output-" + vm_name + "/packer-" + vm_name + ".vmx"
 
-ssh_stdin, ssh_stdout, ssh_stderr = ssh.exec_command(register_vm)
+	ssh_stdin, ssh_stdout, ssh_stderr = ssh.exec_command(register_vm)
